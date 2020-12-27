ea20276aa2e8f8c80f5d35eecb888fc759bb609e
Piotr Kabaciński <piotrkabacinski@users.noreply.github.com>
2020-12-27 21:54:25 +0100

    Heroku's Postgres connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,5 +2,17 @@ import { createConnection, getConnectionOptions } from "typeorm";
 
 export default async function dbConnect() {
   const connectionOptions = await getConnectionOptions();
+
+  // Heroku's Postgres connection
+  if (process.env.NODE_ENV === "production") {
+    Object.assign(connectionOptions, {
+      url: process.env.DATABASE_URL,
+      logging: false,
+      extra: {
+        ssl: true,
+      },
+    });
+  }
+
   return await createConnection(connectionOptions);
 }
