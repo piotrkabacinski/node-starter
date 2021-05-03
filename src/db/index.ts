3ff3fea039de0141bb14cc8f064a3dbde43f2d5f
Piotr Kabaciński <piotrkabacinski@users.noreply.github.com>
2021-05-03 12:10:51 +0200

    Update ormconfig

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,20 +2,5 @@ import { createConnection, getConnectionOptions } from "typeorm";
 
 export default async function dbConnect() {
   const connectionOptions = await getConnectionOptions();
-
-  if (process.env.NODE_ENV === "production") {
-    Object.assign(connectionOptions, {
-      url: process.env.DATABASE_URL,
-      logging: true,
-      // https://github.com/typeorm/typeorm/issues/278#issuecomment-614345011
-      ssl: true,
-      extra: {
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
-    });
-  }
-
   return await createConnection(connectionOptions);
 }
