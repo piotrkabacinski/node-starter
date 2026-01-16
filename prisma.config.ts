6d8586cb4ddafba56053f1490a14bb7d3c2a2d73
Piotr Kabaciński <piotr.kabacinski@codequest.com>
2026-01-16 12:23:39 +0100

    chore: adjust prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from "@prisma/config";
-
-const {
-  POSTGRES_HOST,
-  POSTGRES_DB,
-  POSTGRES_PORT,
-  POSTGRES_USER,
-  POSTGRES_PASSWORD,
-} = process.env;
+import { datasourceUrl } from "./src/db/index";
 
 export default defineConfig({
   schema: "./src/db/schema.prisma",
   datasource: {
-    url: `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?schema=public`,
+    url: datasourceUrl,
   },
 });
