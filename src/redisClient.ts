0415af9942985d2a2a0488b8ef7b43a609f68174
Piotr Kabaciński <piotrkabacinski@users.noreply.github.com>
2026-02-01 17:32:18 +0100

    fix: get redis config

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -1,21 +1,8 @@
 import { createClient } from "redis";
 
-const getConfig = () => {
-  if (process.env.NODE_ENV !== "production") {
-    return {
-      url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
-    };
-  }
-
-  const { port, hostname, href } = new URL(process.env.REDIS_URL);
-
-  return {
-    no_ready_check: true,
-    password: /\/\/:(.*?)@/.exec(href)[1],
-    host: hostname,
-    port: Number(port),
-  };
-};
+const getConfig = () => ({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+});
 
 const client = createClient(getConfig());
 
